9fc8c44def8c6857068512cf9f3ecdc942f9aeb5
huckiyang <huckiyang@gmail.com>
2020-05-28 16:20:07 -0400

    eval tf lite

---
+++ mov.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+ThisPath = '/home/ma-prof/Group/huck/0_Speech/data_2020/tau-2020-Task-1b/'
+train_csv = ThisPath + 'evaluation_setup/fold1_train.csv'
+val_csv = ThisPath + 'evaluation_setup/fold1_evaluate.csv'
+feat_path = '/home/ma-prof/Group/huck/0_Speech/DCASE_2020_Task1/2020_1b_features'
+experiments = 'exp_2020_1b_test_mobnet'
+
+def load_data_2020(feat_path, csv_path, feat_dim, file_type):
+    with open(csv_path, 'r') as text_file:
+        lines = text_file.read().split('\n')
+        for idx, elem in enumerate(lines):
+            lines[idx] = lines[idx].split('\t')
+            lines[idx][0] = lines[idx][0].split('/')[-1].split('.')[0]
+
+        # remove first line
+        lines = lines[1:]
+        lines = [elem for elem in lines if elem != ['']]
+        for idx, elem in enumerate(lines):
+            lines[idx][-1] = lines[idx][-1].split('\r')[0]
+        label_info = np.array(lines)
+        
+        data_df = pd.read_csv(csv_path, sep='\t', encoding='ASCII')
+        ClassNames = np.unique(data_df['scene_label'])
+        labels = data_df['scene_label'].astype('category').cat.codes.values
+
+        feat_mtx = []
+        for [filename, labnel] in label_info:
+            filepath = feat_path + '/' + filename + '.' + file_type
+            with open(filepath,'rb') as f:
+                temp=pickle.load(f, encoding='latin1')
+                feat_mtx.append(temp['feat_data'])
+
+        feat_mtx = np.array(feat_mtx)
+
+        return feat_mtx, labels
+
+
+LM_val, y_val = load_data_2020(feat_path, val_csv, 128, 'logmel')
+
+print(LM_val[0:1100].shape, "--LM----")
+
+#for n in range(0, 1000):
+for n in range(7):
+  k = 150*n
+  LM_co = LM_val[0+k:150+k]
+  np.save('LM_co_'+str(k), LM_co)
+  print(y_val[0+k:150+k].shape, "--y----")
+
+y_co = y_val[0:1100]
+np.save('y_co', y_co)
+print(y_val[0:3])
